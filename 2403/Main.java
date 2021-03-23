abaff5d03d6f272770f3c464b7718e24a562803b
thxnk <grudin.svs@gmail.com>
2021-03-23 18:56:55 +0200

    just

--- 2403/Main.java
+++ 2403/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        JFrame frame = new JFrame("SomeThing");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        JLabel label = new JLabel("Test");
+        frame.add(label);
     }
 }
