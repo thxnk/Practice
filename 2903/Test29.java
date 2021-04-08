6a65a39a08e5018fa9c5194d3ded68a152e1d280
thxnk <grudin.svs@gmail.com>
2021-04-08 14:56:48 +0300

    just

--- 2903/Test29.java
+++ 2903/Test29.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-public class Test {
+public class Test29 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         CCol ccol = new CCol();
         for(int i = 0; i <= 2; i++)
             ccol.addC();
